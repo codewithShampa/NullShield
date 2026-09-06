3629f067f3be4f8c25e479e0fb17360eaf04a94b
codewithShampa <shampa.laakeview@gmail.com>
2026-09-06 02:49:01 +0000

    feat(core): Setup React root and base styles

---
+++ frontend/src/App.tsx
@@ -0,0 +1,37 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { WalletProvider } from './contexts/WalletContext';
+import { ThemeProvider } from './contexts/ThemeContext';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import LandingPage from './pages/LandingPage';
+import VotePage from './pages/VotePage';
+import ResultsPage from './pages/ResultsPage';
+import AdminPage from './pages/AdminPage';
+import AboutPage from './pages/AboutPage';
+import './index.css';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <WalletProvider>
+        <Router>
+          <div className="app-container">
+            <NavBar />
+            <main className="main-content">
+              <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/vote" element={<VotePage />} />
+                <Route path="/results" element={<ResultsPage />} />
+                <Route path="/admin" element={<AdminPage />} />
+                <Route path="/about" element={<AboutPage />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </WalletProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
